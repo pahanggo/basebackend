bc4500924e6c65369b3fc2202c71f5ee5f10a1b0
Zulfa Juniadi Zulkifli <zulfajuniadi@gmail.com>
2024-04-30 15:25:13 +0800

    Update welcome theme

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{config('app.name')}}</title>
-    <link rel="stylesheet" href="/packages/backpack/base/css/bundle.css">
+    @include(backpack_view('inc.head'))
 </head>
 <body>
     <div class="container pt-5 mb-5">
